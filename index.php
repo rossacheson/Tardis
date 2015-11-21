ad400123feaba9d61222561edac2e399897fd931
rossacheson <ross.acheson@gmail.com>
2015-11-21 15:17:32 -0500

    Initialize

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+	/*
+	$mysqli = new mysqli("localhost", "rdadesig_test1", "Make great tables"); 
+
+	
+
+	$sql = "SHOW DATABASES;";
+
+	$result = $mysqli->query($sql);
+
+	
+	
+	while($row = $result->fetch_assoc()) {
+
+		echo $row['Database'] . '<br/>'; 
+
+	}
+	
+	
+
+	$result->free(); $mysqli->close();
+	*/
+
+	echo 'Hi Nesseya and Fengxi!';
+?>
