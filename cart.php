2d5572cc18c5fc7ffc152b278ce3b38b8250cf19
lzy14 <2282644529@qq.com>
2015-12-08 01:12:43 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -62,20 +62,49 @@ $arr=$_SESSION["mycart"];
 				<td width="158"><?php echo $a["name"]?></td>
 				<td width="154"><?php echo $a["num"]?>
 				<form name="form1" enctype="multipart/form-data" method="post" action="modify.php?id=<?php echo @$a[product_ID]?>">
-					<label>
-						<select name="select">
-							<option value="1">1</option>
-							<option value="2">2</option>
-							<option value="3">3</option>
-							<option value="4">4</option>
-							<option value="5">5</option>
-							<option value="6">6</option>
-							<option value="7">7</option>
-							<option value="8">8</option>
-							<option value="9">9</option>
-							<option value="10">10</option>
-						</select>
-					</label>
+					 <label>
+                                                <select name="select1">
+                                                        <option value="0">0</option>
+                                                        <option value="1">1</option>
+                                                        <option value="2">2</option>
+                                                        <option value="3">3</option>
+                                                        <option value="4">4</option>
+                                                        <option value="5">5</option>
+                                                </select>
+                                        </label>
+
+                                         <label>
+                                                 <select name="select2">
+                                                         <option value="0">0</option>
+                                                         <option value="1">1</option>
+                                                         <option value="2">2</option>
+                                                         <option value="3">3</option>
+                                                         <option value="4">4</option>
+                                                         <option value="5">5</option>
+                                                         <option value="6">6</option>
+                                                         <option value="7">7</option>
+                                                         <option value="8">8</option>
+                                                         <option value="9">9</option>
+                                                  </select>
+                                         </label>
+
+                                         <label>
+                                                 <select name="select3">
+                                                         <option value="1">1</option>
+                                                         <option value="2">2</option>
+                                                         <option value="3">3</option>
+                                                         <option value="4">4</option>
+                                                         <option value="5">5</option>
+                                                         <option value="6">6</option>
+                                                         <option value="7">7</option>
+                                                         <option value="8">8</option>
+                                                         <option value="9">9</option>
+                                                         <option value="0">0</option>
+                                                </select>
+                                        </label>
+
+
+
 					<label>
 						<input type="submit" name="Submit" value="modify">
 					</label>
