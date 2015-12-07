69b34d2bb4b969f853d07e6791dc0054a121d023
mentosforgithub <mentos.vicky@gmail.com>
2015-12-07 11:51:54 -0500

    Update store.php

--- store.php
+++ store.php
@@ -5,11 +5,7 @@ ob_start();
 
 $Store_id=$_POST("Store_ID");
 
-$store="select Store_ID from store where Store_ID =".$Store_id;
-$store_result=mysql_query($store, $conn);
-
-
-$salesperson="select name,employee_ID from employee where job_title='salesperson'and store_ID=".$Store_id;
+$salesperson="select employee_ID from employee where job_title='salesperson'and store_ID=".$Store_id;
 $salesperson_result=  mysql_query($salesperson, $conn);
 
 while($row = mysql_fetch_array($salesperson_result))
