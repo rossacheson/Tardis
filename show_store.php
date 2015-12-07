f61606573167241d725552175b62f8a970e694ce
mentosforgithub <mentos.vicky@gmail.com>
2015-12-07 11:07:31 -0500

    Update show_store.php

--- show_store.php
+++ show_store.php
@@ -4,4 +4,8 @@ include ("conn.php");
 $store_show="select Store_ID from store";
 $store_show_result=mysql_query($store_show, $conn);
 
-echo "Store ID:+{$store_show_result}";
+while($row = mysql_fetch_array($store_show_result))
+  {
+  echo $row['store_ID'];
+  echo "<br />";
+  }
