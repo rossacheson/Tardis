c0ebddc02fc07a987834d70ffffb70e1c6d7a0a4
mentosforgithub <mentos.vicky@gmail.com>
2015-12-07 11:09:05 -0500

    Update store.php

--- store.php
+++ store.php
@@ -12,7 +12,11 @@ $store_result=mysql_query($store, $conn);
 $salesperson="select name,employee_ID from employee where job_title='salesperson'and store_ID=".$Store_id;
 $salesperson_result=  mysql_query($salesperson, $conn);
 
-echo $salesperson_result;
+while($row = mysql_fetch_array($salesperson_result))
+  {
+  echo $row['salesperson_ID'];
+  echo "<br />";
+  }
 
 ob_clean();
 
