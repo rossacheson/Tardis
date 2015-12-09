37f6c1361e006e93a72bc27523ae1bd304387f7d
lzy14 <2282644529@qq.com>
2015-12-09 12:14:14 +0800

    Update pay.php

--- pay.php
+++ pay.php
@@ -43,86 +43,147 @@
 	</header>
 	<section class="maincontent">
 		<?php
-			include ("conn.php");
-			//echo "sfe";
-
-			$customer_ID=$_POST["customer_ID"];
-			echo $customer_ID;
-			$salesperson_ID=$_POST["salesperson_ID"];
-			$date=date("Y/m/d");
-			echo $salesperson_ID;
-			echo $date;
+include ("conn.php");
 
 
+$customer_ID=$_POST["customer_ID"];
+if(!$customer_ID)
+{
+	echo "<P><a href='index.html'>Back to homepage</a></p>";
+		die ("please type you ID" );
 
-			  $get_transactionID="select max(transaction_ID) as max_transaction from transaction ";
-				$transaction_ID=mysql_query($get_transactionID, $conn);
-				/*$sdf=$transaction_ID+1;*/
-				while($row=mysql_fetch_array($transaction_ID))
-						{
-							$transaction_number=$row["max_transaction"];
-				echo  $transaction_number;
-						}
-						$transaction_insert=$transaction_number+1;
-						
-						
-						
-						
-						mysql_query("INSERT INTO transaction(transaction_ID,salesperson,customer_ID,date) VALUES ($transaction_insert,'$salesperson_ID','$customer_ID','$date')");
+}
 
-			  $get_buyID="select max(buy_ID) as max_buy from buy ";
-				$buy_ID=mysql_query($get_buyID, $conn);
-				/*$sdf=$transaction_ID+1;*/
-				while($row=mysql_fetch_array($buy_ID))
-						{
-							$buy_number=$row["max_buy"];
-				echo  $buy_number;
-						}
-						$buy_insert=$buy_number+1;
-						
-						
-						
-						
-						
-			session_start();
-			ob_start();
-			$arr=$_SESSION["mycart"];
+    $get_cusID="select *  from customer where customer_ID='$customer_ID'";
+    $cus_ID=mysql_query($get_cusID, $conn);
+	
+	if(!$cus_ID)
+	{
+		  echo "<P><a href='index.html'>Back to homepage</a></p>";
+			die( "we do not have this customer");
+		}
+		
+		
 
 
-			$total_money=0;
+$transaction_insert=1;
 
-			foreach ($arr as $a) {
-				$Product_in_cart_ID=$a["product_ID"];
-				$Product_in_cart_name=$a["name"];
-				$Product_in_cart_num=$a["num"];
+$salesperson_ID=$_POST["salesperson_ID"];
+if(!$salesperson_ID)
+{
+	echo "<P><a href='index.html'>Back to homepage</a></p>";
+		die ("please choose a salesperson");
 
-				$price="select price from product where product_ID ='$Product_in_cart_ID'";
+}
+
+
+ $get_saID="select * from employee where job_title='Salesperson' and employee_ID='$salesperson_ID' ";
+    $sa_ID=mysql_query($get_saID, $conn);
+	if(!$sa_ID)
+	{
+		echo "<P><a href='index.html'>Back to homepage</a></p>";
+			die( "we do not have this salesperson");
+		}
+
+
+
+
+$systemdate=DATE("Y/m/d");
+
+
+
+
+  $get_transactionID="select max(transaction_ID) as max_transaction from transaction ";
+    $transaction_ID=mysql_query($get_transactionID, $conn);
+	/*$sdf=$transaction_ID+1;*/
+
+		while($row=mysql_fetch_array($transaction_ID))
+	{
+		if($row["max_transaction"])
+		{
+				$transaction_number=$row["max_transaction"];
+			$transaction_insert=$transaction_number+1;
+	    }
 				
-				$price_paid=mysql_query($price, $conn);
-			  while($row=mysql_fetch_array($price_paid))
-						{
-							$price_transaction=$row["price"];
+	}
 
-						}
-			  
-			  
-			  
-				
-				$product_paid=$price_transaction*$Product_in_cart_num;
-				$total_money=$total_money+$product_paid;
-				
-			   
-				
-				mysql_query("INSERT INTO buy(transaction_ID,buy_ID,product_ID,price_paid,quantity) VALUES ($transaction_insert,$buy_insert,'{$Product_in_cart_ID}','{$product_paid}','{$Product_in_cart_num}')");
-				
+
+		
+			
+session_start();
+ob_start();
+$arr=$_SESSION["mycart"];
+
+
+
+			
+$total_money=0;
+
+foreach ($arr as $a) {
+    $Product_in_cart_ID=$a["product_ID"];
+    $Product_in_cart_name=$a["name"];
+    $Product_in_cart_num=$a["num"];
+
+    $price="select price from product where product_ID ='$Product_in_cart_ID'";
+	
+    $price_paid=mysql_query($price, $conn);
+  while($row=mysql_fetch_array($price_paid))
+			{
+				$price_transaction=$row["price"];
+
 			}
+  
+  
+    
+    $product_paid=$price_transaction*$Product_in_cart_num;
+    $total_money=$total_money+$product_paid;
+	
+    
+    $get_buyID="select max(buy_ID) as max_buy from buy ";
+    $buy_ID=mysql_query($get_buyID, $conn);
+	
+	while($row=mysql_fetch_array($buy_ID))
+			{
+				$buy_number=$row["max_buy"];
+    echo  $buy_number;
+			}
+			$buy_insert=$buy_number+1;
+	if($total_money)
+	{
+		
+		
+    mysql_query("INSERT INTO buy(buy_ID,product_ID,price_paid,quantity) VALUES ($buy_insert,'$Product_in_cart_ID','$price_transaction','$Product_in_cart_num')");
+
+    
+    mysql_query("INSERT INTO transaction(transaction_ID, buy_ID,salesperson,customer_ID,date) VALUES ($transaction_insert,$buy_insert,'$salesperson_ID','$customer_ID','$systemdate')");
+	}
+}
 
 
-			session_destroy();
-			ob_clean();
+session_destroy();
+ob_clean();
+if($total_money==0)
+{
+	echo "you do not choose anything";
+	 echo "<P><a href='index.html'>Back to homepage</a></p>";
+}
+if($total_money!=0)
+{
+	echo "The total money to pay is {$total_money} dollars";
+    echo "<P><a href='index.html'>Back to homepage</a></p>";
+}
 
-			echo "The total money to pay is {$total_money}";
-		?>
+
+?>
+
+
+
+
+
+
+
+
+
 	</section>
   </body>
 </html>
