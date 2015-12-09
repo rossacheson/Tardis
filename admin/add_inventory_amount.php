6acf1709e953dadca630d47a433d7813b7d9d2f6
lzy14 <2282644529@qq.com>
2015-12-09 13:09:55 +0800

    Update add_inventory_amount.php

--- admin/add_inventory_amount.php
+++ admin/add_inventory_amount.php
@@ -44,24 +44,34 @@
 		<?php
 		include ("../conn.php");
 
-		$product_ID=$_POST["product_ID"]; 
+	$product_ID=$_POST["product_ID"]; 
 		$store_ID=$_POST["store_ID"];
 		$add_amount=$_POST["add_amount"];
 
 		$product_check="select * from inventory_amount where product_ID ='$product_ID' AND store_ID='$store_ID'";
 		$result=mysql_query($product_check, $conn);
 
-		if($result!=null){
+		if($result+$add_amount>0)
+		{
+			if($result!=null)
+			{
 			mysql_query("UPDATE inventory_amount SET inventory_amt=inventory_amt+$add_amount WHERE product_ID ='$product_ID' AND store_ID='$store_ID'");
 			echo "<p>Added $add_amount units of $product_ID to store $store_ID</p>";
 		}
+		}
+		if($result+$add_amount<0)
+		{
+			echo "Sorry, we don't have enough products";
+		}
+		
+		
 
 		else{
 			mysql_query("INSERT INTO inventory_amount(product_ID,store_ID,inventory_amt) VALUES ('{$product_ID}','{$store_ID}','{$add_amount}')");
 
 		}
 			
-		mysql_close($conn);   
+		mysql_close($conn);    
 		?>
 	</section>
   </body>
